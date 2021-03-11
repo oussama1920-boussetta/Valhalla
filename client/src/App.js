e87d9078a567c0211d87d73d3ab05ad6abd5e1df
oussama1920-boussetta <boussettamohamedoussama@gmail.com>
2021-03-11 11:08:14 +0100

    css2

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Register from "./components/Register";
 import Profile from "./components/Profile";
 import { getProfile } from "./JS/actions";
 import PrivateRoute from "./components/PrivateRoute";
-
+import Nav from './components/Nav'
 const App = () => {
   const dispatch = useDispatch();
   const isAuth = useSelector((state) => state.userReducer.isAuth);
@@ -19,11 +19,22 @@ const App = () => {
 
   return (
     <div className="App">
+     
+     
+
+
+
       <Switch>
+      
         <Route exact path="/" render={(props) => <Register {...props} />} />
         <Route exact path="/login" render={(props) => <Login {...props} />} />
+       
         <PrivateRoute exact path="/profile" component={Profile} />
+       
       </Switch>
+
+
+      <Nav/>
     </div>
   );
 };
---
+++ client/src/components/Nav.js
@@ -0,0 +1,60 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Carousel from 'react-bootstrap/Carousel'
+import React from 'react'
+import valhalla from '../components/images/valhalla.png'
+import block from '../components/images/block.jpg'
+import vrgaming from '../components/images/vrgaming.jpg'
+import vrvr from '../components/images/vrvr.jpg'
+function Nav() {
+    return (
+        <div className='car'>
+<Carousel >
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={vrvr}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+    <img className='val'
+      src={valhalla}
+      alt=""
+    />
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={block}
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+    <img className='val'
+      src={valhalla}
+      alt=""
+    />
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={vrgaming}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+    <img className='val'
+      src={valhalla}
+      alt=""
+    />
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</div>
+    )
+}
+
+export default Nav
