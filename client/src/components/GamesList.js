73380917a0f68a0e3c5c81ae2671898f47adc977
Rchid-Baccouchi <72676179+$Rchid-Baccouchi@users.noreply.github.com>
2021-03-15 13:06:16 -1200

    added images

--- client/src/components/GamesList.js
+++ client/src/components/GamesList.js
@@ -2,7 +2,13 @@ import React from 'react'
 import { getGames } from '../JS/actions/gameAction';
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import theSecret from '../components/images/thesecret.jpg'
+import deadandBuried from '../components/images/Dead&Buried.jpg'
+import angryBirds from '../components/images/angrybirds.jpg'
+import beatSaber from '../components/images/beastSaber.jpg'
 
+const imagesArr= [theSecret,deadandBuried,angryBirds,beatSaber]
+console.log(imagesArr)
 const GamesList = () => {
     const dispatch = useDispatch();
     const games = useSelector((state) => state.gameReducer.games);
@@ -14,8 +20,9 @@ const GamesList = () => {
       }, []);
     return (
         <div>
-            {games.map((game)=>(<ul>
+            {games.map((game,index)=>(<ul>
                 <li>{game.title}</li>
+                <img width='300' src={imagesArr[index]}/>
                 <li>{game.description}</li>
                 <li>{game.price}</li>
             </ul>  ))}
