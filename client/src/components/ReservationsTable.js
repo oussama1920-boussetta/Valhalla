45eff0bf7ec90a8ad5490d4dfb2c4c234fdf61cc
Rchid-Baccouchi <72676179+$Rchid-Baccouchi@users.noreply.github.com>
2021-03-15 12:16:55 -1200

    new changes on profile

--- client/src/components/ReservationsTable.js
+++ client/src/components/ReservationsTable.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import { getReservations } from '../JS/actions/reservationAction';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const ReservationsTable = ({reservation}) => {
-    console.log(reservation)
+const ReservationsTable = () => {
+    const dispatch = useDispatch();
+    const reservations = useSelector((state) => state.reservationReducer.reservations);
+    const getAllReservations = () => {
+      dispatch(getReservations());
+    };
+  
+    useEffect(() => {
+      getAllReservations();
+    }, []);
+  
     return (
         <div>
-            <ul>
+           {reservations.map((reservation,index)=>( 
+           <ul>
                 <li>{reservation.userName}</li>
                 <li>{reservation.date}</li>
                 <li>{reservation.time}</li>
                 <li>{reservation.price}</li>
                 <li>{reservation.status}</li>
-            </ul>
+            </ul>))}
         </div>
     )
 }
--- server/models/Game.js
+++ server/models/Game.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const gameSchema = mongoose.Schema({
   title: String,
   description:String,
-  price:Number
+  price:Number,
 });
 
 module.exports = Game = mongoose.model("game", gameSchema);
